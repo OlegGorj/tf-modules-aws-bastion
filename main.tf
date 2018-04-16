bbbab3576e90fc2b564f82c026ec64dcc3d45157
Oleg Gorodnitchi <oleg.gorodnitchi@sk.ibm.com>
2018-04-16 19:45:26 +0200

    latest ubuntu base image

--- main.tf
+++ main.tf
@@ -20,8 +20,100 @@ variable "ip_allow5" {}
 ###############################################################################
 # RESOURCES
 ###############################################################################
+// Get us the newest Ubuntu base ami
+data "aws_ami" "ubuntu" {
+    most_recent = true
 
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    }
 
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+
+    filter {
+      name   = "owner-id"
+      values = ["099720109477"] # Canonical
+    }
+}
+
+resource "aws_iam_role" "instance_role" {
+
+  name = "${var.env}-bastion-instance-role"
+  path = "/"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "instance_policy" {
+  name = "${var.env}-bastion-instance-role-policy"
+  role = "${aws_iam_role.instance_role.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.state_bucket}",
+                "arn:aws:s3:::${var.state_bucket}/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "S3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${var.state_bucket}/ssh/${var.env}/machine-user/*",
+                "arn:aws:s3:::${var.state_bucket}/ssh/${var.env}/common/*",
+                "arn:aws:s3:::${var.state_bucket}/ansible/${var.env}/bastion/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:Describe*",
+                "route53:ListHostedZones",
+                "route53:ListResourceRecordSets",
+                "elasticache:Describe*"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+
+  # Allows the IAM role enough time to propagate through AWS
+  provisioner "local-exec" {
+    command = <<EOT
+            echo "Sleeping for 10 seconds to allow the IAM role enough time to propagate through AWS";
+            sleep 10;
+            echo "IAM role should be propagated, proceeding.";
+EOT
+  }
+}
 
 
 ###############################################################################
